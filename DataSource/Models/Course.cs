a91d09277351328b05a9d1d849ea5bc38a77ffb3
natipro22 <natipro22@github.com>
2023-01-21 21:19:21 +0300

    Add catagory column to courses table

--- DataSource/Models/Course.cs
+++ DataSource/Models/Course.cs
@@ -9,6 +9,7 @@ namespace DataSource.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public Catagory Catagory { get; set; }
         public CourseLevel CourseLevel { get; set; }
         public float FullPrice { get; set; }
         public Author Author { get; set; }
---
+++ EntityFrameworkCodeFirst/Migrations/202301211817144_AddCatagoryColumnToCoursesTable.cs
@@ -0,0 +1,22 @@
+﻿namespace EntityFrameworkCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddCatagoryColumnToCoursesTable : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Courses", "Catagory_Id", c => c.Int());
+            CreateIndex("dbo.Courses", "Catagory_Id");
+            AddForeignKey("dbo.Courses", "Catagory_Id", "dbo.Catagories", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Courses", "Catagory_Id", "dbo.Catagories");
+            DropIndex("dbo.Courses", new[] { "Catagory_Id" });
+            DropColumn("dbo.Courses", "Catagory_Id");
+        }
+    }
+}
