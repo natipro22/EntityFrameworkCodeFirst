402dec666336c0e3d6d05bdf0137a148ff31d42f
natipro22 <natipro22@github.com>
2023-01-21 20:58:02 +0300

    Seed Catagories table with sample data

---
+++ EntityFrameworkCodeFirst/Migrations/202301211750289_SeedCatagoriesTableWithData.cs
@@ -0,0 +1,19 @@
+﻿namespace EntityFrameworkCodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SeedCatagoriesTableWithData : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO Catagories (Name) VALUES ('Web Development')");
+            Sql("INSERT INTO Catagories (Name) VALUES ('Programming Languages')");
+        }
+        
+        public override void Down()
+        {
+
+        }
+    }
+}
